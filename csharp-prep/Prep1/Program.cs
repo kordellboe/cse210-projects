7e38052e77b5c210ac97a1308a7c520c7fb26770
kordellboe <kordellboehning1@gmail.com>
2025-10-13 10:40:31 -0600

    Updated

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        String firstname = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        String lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
     }
+
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Whats your precentage?");
+        string gradePerecentage = Console.ReadLine();
+        int grade = int.Parse(gradePerecentage);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            //  Console.WriteLine("You got an A");
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            //Console.WriteLine("You got a B");
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            //Console.WriteLine("You got a C");
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            //Console.WriteLine("You got a D");
+            letter = "D";
+        }
+        else
+        {
+            //Console.WriteLine("You got an F");
+            letter = "F";
+        }
+        Console.WriteLine($"Your letter grade is {letter}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time");
+        }
+
+
     }
 }
